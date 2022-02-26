1103b4b8d175dc1a58e73dbc9c46398e0a383926
gaabriel <xgabrieldev@gmail.com>
2022-02-26 17:32:25 -0300

    Update to 0.6

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Dependencies {
-    const val ProjectVersion = "0.5-SNAPSHOT"
+    const val ProjectVersion = "0.6-SNAPSHOT"
     const val KotlinVersion = "1.6.0"
 
     const val DeckVersion = "0.0.4-BETA"
