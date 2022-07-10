fdbd2e75e249a3ada78f5e02f65cbddafe1f06a2
SrGaabriel <58668092+xGabrielDEV@users.noreply.github.com>
2022-07-10 11:32:52 -0300

    chore: whoops, remove debug stuff

--- clubs-common/src/commonMain/kotlin/parser/DefaultCommandParser.kt
+++ clubs-common/src/commonMain/kotlin/parser/DefaultCommandParser.kt
@@ -27,38 +27,29 @@ public class DefaultCommandParser(
         val argumentValues = mutableMapOf<CommandArgumentNode<*, *>, Any>()
 
         fun scanThroughBranch(root: Command<*>, node: CommandNode<*>, reader: StringReader): CommandNode<*>? {
-            println("Now scanning through another node ($node)")
             if (node.children.isEmpty() || reader.isEnd())
                 return node
-            println("Node confirmed to have children.")
             val peek = reader.peek()
             val literalWithName = node.children.firstOrNull { it is CommandLiteralNode<*> && it.name.equals(peek, ignoreCase = true) }
-            println("Literal with name for node is: $literalWithName")
             if (literalWithName != null) {
                 reader.next()
-                println("There is a literal with name $peek after all!")
                 return scanThroughBranch(root, literalWithName, reader)
             }
-            println("There isn't a literal with name $peek")
 
-            println("Checking for argument nodes for $peek!")
             val argumentNodes = node.children.filterIsInstance<CommandArgumentNode<*, *>>()
             if (argumentNodes.isEmpty())
                 return null
             else if (argumentNodes.size == 1) {
-                println("There was only one possible argument for $peek!")
                 val argumentFound = argumentNodes.first()
                 argumentValues[argumentFound] = argumentFound.type.parse(reader, dictionary)
                 return scanThroughBranch(root, argumentFound, reader)
             }
-            println("Multiple possible arguments found for $peek!")
 
             // there have to be at least two arguments
             var desiredOutcome: CommandNode<*>? = null
             val validArgumentNode = argumentNodes.firstOrNull {
                 it.type.isParseable(reader) && scanThroughBranch(root, node, reader)?.also { outcome -> desiredOutcome = outcome } != null
             }
-            println("Not any argument was found! :(")
             if (desiredOutcome == null || validArgumentNode == null)
                 return null
 
